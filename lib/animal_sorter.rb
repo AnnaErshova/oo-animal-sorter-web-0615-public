a58552e85ab8574a2ad2d54fb57c2fa18093deca
AnnaErshova <anna.ershova@gmail.com>
2015-06-18 21:41:29 -0400

    refactoring code

--- lib/animal_sorter.rb
+++ lib/animal_sorter.rb
@@ -12,11 +12,6 @@
 # should include the sea creatures, the second, land animals.
 
 class AnimalSorter
-
-  # attr_accessor :SEA_ANIMALS # this is an array
-  # attr_accessor :LAND_ANIMALS # this is an array
-  # attr_accessor :sea_animals, :land_animals, :animal_array
-  
   SEA_ANIMALS = ["marlin", "octopus", "fish"]
   # http://en.wikipedia.org/wiki/Aardvark
   LAND_ANIMALS = ["aardvark", "cat", "elephant"]
@@ -26,12 +21,10 @@ class AnimalSorter
     @animal_array = animal_array
   end
 
-  def to_a
+  def to_a # this is really just iterating over given arrays, sorting and pushing into a new array once sorted
     sea_animals = Array.new
     land_animals = Array.new
-
     @animal_array.each {|animal| SEA_ANIMALS.include?(animal) ? sea_animals << animal : land_animals << animal }       
-
     [sea_animals,land_animals] # explicit array with sea_animals first
     # OR return sea_animals,land_animals
     # sea creatures first, land creatures second
